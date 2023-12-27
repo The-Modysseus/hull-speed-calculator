aebed95e73fe2e507cf9787a867d8812864356b1
Mohamad Farhoud <Mohamadfarhoud@hotmail.com>
2023-12-27 21:49:04 +0100

    Change Sailboat class

--- src/sailboat.js
+++ src/sailboat.js
@@ -1,10 +1,19 @@
-class Sailboat{
-    constructor(name, length){
-        this.name = name;
-        this.length = length;
-    }
-    
-    getHullSpeed(){
+export default function Sailboat() {
+    var _name;
+    var _length;
+
+    Object.defineProperty(this, 'name', {
+        get: function() { return _name; },
+        set: function(name) { _name = name; }
+    });
+
+    Object.defineProperty(this, 'length', {
+        get: function() { return _length; },
+        set: function(length) { _length = length; }
+    });
+
+    // Member functions
+    this.hullspeed = function() {
         return 1.34 * Math.sqrt(this.length);
-    }
+    };
 }
